fcce70869c32fb518e2f803c72ff5af248ef59bb
ljones1877 <jlorin7@gmail.com>
2019-12-13 21:39:47 +0000

    l19 weird factorybot error

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
   end
 
   factory :gram do
-    message { "Hello!" }
+    message { "hello" }
     picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
 
     association :user
