a1d842b2586055407ef84c4da2022c77361afab0
ljones1877 <jlorin7@gmail.com>
2019-12-14 00:08:21 +0000

    l19 fixed the carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,9 @@
 CarrierWave.configure do |config|
-  
+  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-      access_key_id:     ENV["AWS_ACCESS_KEY"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      region:            ENV["AWS_REGION"]
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-
-  # Optional: Signing of download urls, e.g. for serving private content through
-  # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
-  # configured:
-  # config.aws_signer = -> (unsigned_url, options) do
-  #   Aws::CF::Signer.sign_url(unsigned_url, options)
-  # end
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
